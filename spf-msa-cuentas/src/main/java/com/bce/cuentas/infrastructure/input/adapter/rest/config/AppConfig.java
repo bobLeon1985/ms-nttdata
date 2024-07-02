4f8e407bae0afd0de0770162b08365f7c81ec64a
Edwin Leon <orlando.leon@ucuenca.edu.ec>
2024-07-02 01:26:08 -0500

    Fix: ajustes

--- spf-msa-cuentas/src/main/java/com/bce/cuentas/infrastructure/input/adapter/rest/config/AppConfig.java
+++ spf-msa-cuentas/src/main/java/com/bce/cuentas/infrastructure/input/adapter/rest/config/AppConfig.java
@@ -1,13 +1,20 @@
 package com.bce.cuentas.infrastructure.input.adapter.rest.config;
 
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class AppConfig {
+
+    @NotBlank
+    @Value("${client.http-client.baseUrl}")
+    String baseUrl;
+
     @Bean
     public WebClient registrarWebClient() {
-        return WebClient.create("http://localhost:8081/msclient/v1");
+        return WebClient.create(baseUrl);
     }
 }
